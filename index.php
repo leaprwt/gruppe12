953253113631ba54b09e3a5cafb597db4146618b
learxtz <117345124+learxtz@users.noreply.github.com>
2022-12-16 10:51:07 +0100

    Update index.php

--- index.php
+++ index.php
@@ -54,14 +54,6 @@ $con->close();
 	
 	<input type="datetime-local" value="">
 	
-	<?php
-	
-$d = time();                   
-$d = strtotime("+1 days", $d); 
-echo date("d.m.Y h:i:s", $d);
-	
-	
-	?>
 	
 	
 	<nav> 
