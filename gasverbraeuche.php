022ee4c2b924021cb3090fa3d2a187e463fec625
learxtz <117345124+learxtz@users.noreply.github.com>
2022-12-16 10:51:27 +0100

    Update gasverbraeuche.php

--- gasverbraeuche.php
+++ gasverbraeuche.php
@@ -1,10 +1,8 @@
-<!doctype html>
-<html>
-<head>
-<meta charset="utf-8">
-<title>Gasverbräuche</title>
-</head>
-
-<body>
-</body>
-</html>
+<?php
+	
+$d = time();                   
+$d = strtotime("+1 days", $d); 
+echo date("d.m.Y h:i:s", $d);
+	
+	
+	?>
