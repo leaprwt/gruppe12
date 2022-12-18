1667fd515589cf4518957515efbac2fc1007a2ab
learxtz <117345124+learxtz@users.noreply.github.com>
2022-12-18 13:36:12 +0100

    Create header.php

---
+++ header.php
@@ -0,0 +1,43 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Header</title>
+	
+<link href="stylesheet.css" rel="stylesheet" type="text/css">
+</head>
+
+	<div class="header">
+		<h1>Strom- und Gasverbräuche</h1>
+			 
+			 <style>     /* Hintergrundbild verlinkung funktioniert im CSS nicht, darum im php verlinkt */
+
+body {	
+  background-image: url('bild1.jpeg');
+	    min-height: 100vh;
+    width: 100%;
+    background-image: linear-gradient(rgba(4,9,30,0.7), rgba(4,9,30,0.7)), url(bild1.jpeg);
+    background-position:center;
+    background-size:cover;
+    position:relative;
+}
+</style>
+		
+	
+	
+	<nav> 
+		
+		<ul id="navibereich">
+  	<li id="navi01"><a href="gasverbraeuche.php">Gasverbräuche</a></li>
+  	<li id="navi02"><a href="stromverbraeuche.php">Stromverbräuche</a></li>
+	<li id="navi03"><a href="kategorien.php">Kategorien</a></li>
+	<li id="navi04"><a href="gaseinsparen.php">Tipps Gas einsparen</a></li>
+	<li id="navi05"><a href="stromeinsparen.php">Tipps Strom einsparen</a></li>
+	<li id="navi06"><a href="auswertunggas.php">Auswertung Gasverbrauch</a></li>
+	<li id="navi07"><a href="auswertungstrom.php">Auswertung Stromverbrauch</a></li>
+</ul>
+	
+	</nav>
+
+</body>
+</html>
