cd06a0266dc9664f257dba26e6eba1fd3196fe58
learxtz <117345124+learxtz@users.noreply.github.com>
2022-12-18 12:02:45 +0100

    Update auswertunggas.php

--- auswertunggas.php
+++ auswertunggas.php
@@ -3,8 +3,27 @@
 <head>
 <meta charset="utf-8">
 <title>Auswertung zum Gasverbrauch</title>
+	<?php
+	include("header.php"); 
+	?>
+	
 </head>
 
 <body>
+	
+	<?php
+	
+	$kubikmeter = 5;
+	$brennwert = 4;
+	$zustandszahl=6;
+
+	$ergebnis = $kubikmeter*$brennwert*$zustandszahl;
+	
+	echo "<p>Ihr persönlicher Gasverbrauch liegt bei: ".
+     ($kubikmeter * $brennwert * $zustandszahl) . "</p>";
+?>
+	
+	
+	
 </body>
 </html>
