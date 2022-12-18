e0b062950491912d4595ec65856901a2af93dd41
learxtz <117345124+learxtz@users.noreply.github.com>
2022-12-18 12:04:27 +0100

    Update kategorien.php

--- kategorien.php
+++ kategorien.php
@@ -6,5 +6,15 @@
 </head>
 
 <body>
+	
+<h1>Kategorien</h1>
+
+	
+	<select name="kategorie" title="Kategorie">
+         <option value="Kühlschrank" selected="selected">Kühlschrank</option>
+         <option value="Fernseher">Fernseher</option>
+         <option value="Herd">Herd</option>
+         <option value="Toaster">Toaster</option>
+      </select>
 </body>
 </html>
