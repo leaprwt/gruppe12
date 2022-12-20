b9759d9dee0673c770f32f34caffa2899e6af5be
leaprwt <116566313+leaprwt@users.noreply.github.com>
2022-12-20 22:19:52 +0100

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,24 +1,43 @@
 <?php
+	include('header.php');
+	include 'homelink.inc.php';
+?>  
 
-if(isset($_POST['submit']))
-      {
-          $ablesedatum = $_POST['ablesedatum'];
-          $kwh = $_POST['kwh'];
-          $entstandene = $_POST['entstandene'];
+<?php
+		$ablesedatum = date('Y-m-d G:i:s');
+		$verbrauche = $_POST['verbrauche'];
+		$entstandene = $_POST['entstandene'];
 
-          $con = mysqli_connect("localhost", "m11575-30", "FrLAwd2QV", "m11575_30");
-if (!$con)
-          {
-die("Connection failed!" . mysqli_connect_error());
-          }
-          $query = "INSERT INTO stromverbrauch (id, ablesedatum, kwh, entstandene VALUES $ablesedatum, $kwh, $entstandene";
-          $run = mysqli_query($con, $query);
-if (run)
-          {
-echo"Success";
-          }
-mysqli_close($con);
-      }
+		$con = mysqli_connect("m11575-30.kurs.jade-hs.de", "m11575-30", "FrLAwd2QV", "m11575_30");
+		if (!$con)
+		{
+			die("Connection failed!" . mysqli_connect_error());
+		}
+        try {
+            $query = $sql="INSERT INTO stromverbrauch (ablesedatum, verbrauche, entstandene) VALUES ('$ablesedatum','$verbrauche','$entstandene')";
+		    $run = mysqli_query($con, $query);
+				
+				echo "Ihre Daten für den Stromverbrauch wurden gespeichert." . "<br>";
+				echo "Hier finden Sie Ihre letzten 10 Einträge:" . "<br>";
+				
+			
+        }catch(Exception $e) {
+            echo $e;
+        }
+		
 
+		$sql = "SELECT ablesedatum, verbrauche, entstandene FROM stromverbrauch LIMIT 10";
+		$result = $con->query($sql);
 
+		if ($result->num_rows > 0) {
+	  // output data of each row
+	  while($row = $result->fetch_assoc()) {
+		  echo " Ablesedatum: " . $row["ablesedatum"]. "<br>";
+		  " Verbräuche: " . $row["verbrauche"]. "<br>";
+		  " Entstandene Kosten: " . $row["entstandene"]. "<br>";
+	  }
+	} else {
+	  echo "0 results";
+	}
+	$con->close();
 ?>
