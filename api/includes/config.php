2f2b69f7faffc79bbf3b7ea0bfc8c52184d16e69
leaprwt <116566313+leaprwt@users.noreply.github.com>
2023-12-17 19:43:37 +0100

    Neu config.php
    Neu eingefügt nach Einfügen des Ordner api

---
+++ api/includes/config.php
@@ -0,0 +1,11 @@
+<?php
+   //local enviroment
+   define("HOST", "localhost:8889");
+   define("USER", "root");
+   define("PASSWORD", "root");
+
+   //prod env
+   // define("HOST", "m12242-30.kurs-jade-hs.de");
+   // define("USER", "m12242-30")
+   // define("PASSWORD", "adHUcO_8A")
+?>
