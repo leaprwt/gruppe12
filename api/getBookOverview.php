415f68622179f54bd436dd097a3e92c60eca169a
leaprwt <116566313+leaprwt@users.noreply.github.com>
2023-12-17 19:39:53 +0100

    Create getBookOverview.php

---
+++ api/getBookOverview.php
@@ -0,0 +1,43 @@
+<?php
+     //imports utils
+    require_once "api/includes/config.php";
+    require_once "api/includes/connectDB.php";
+
+
+    if($_SERVER["REQUEST_METHOD"] === "GET") {
+        $dbname = "library";
+        //Connects to database
+        $conn = connectDB(HOST, USER, PASSWORD, $dbname);
+    
+        //Selects book names and cover-image path
+        $sql = "SELECT book_id, title, cover_image_link FROM books";
+    
+        $result = $conn->query($sql);
+    
+        if ($result->num_rows > 0) {
+            while ($row = $result->fetch_assoc()) {
+                $response[] = array(
+                    'book_id' => $row['book_id'],
+                    'book_title' => $row["title"],
+                    'cover_image_link' => $row['cover_image_link']
+
+                );
+            }
+        } else {
+            echo "Keine Ergebnisse gefunden";
+        }
+    
+
+        
+        // Returns response
+        header('Content-Type: application/json');
+        echo json_encode($response);
+    
+        // Closes connection
+        $conn->close();
+    }else {
+        http_response_code(400);
+        echo json_encode(array('error' => 'Ungültige Methode'));
+    }
+
+ ?>
