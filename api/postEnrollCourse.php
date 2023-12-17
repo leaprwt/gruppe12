1048c0420b9c37ed985dbb4e1703ad58d222d39f
leaprwt <116566313+leaprwt@users.noreply.github.com>
2023-12-17 19:42:54 +0100

    Create postEnrollCourse.php

---
+++ api/postEnrollCourse.php
@@ -0,0 +1,42 @@
+<?php
+    //imports utils
+    require_once "api/includes/config.php";
+    require_once "api/includes/connectDB.php";
+
+    if($_SERVER["REQUEST_METHOD"] === "POST") {
+        $dbname = "studyplaner";
+        //Connects to database
+        $conn = connectDB(HOST, USER, PASSWORD, $dbname);
+    
+
+        //Get json from request
+        $jsonData = file_get_contents('php://input');
+        $data =json_decode($jsonData, true);
+
+
+        $course_id = $data['course_id'];
+        $student_id = $data['student_id'];
+        //gets all courses of a specific student
+        $sql = "INSERT INTO `linking-courses-students` 
+                (course_id, student_id) 
+                VALUES ($course_id, $student_id)";
+
+        $result = $conn->query($sql);
+    
+        if ($result) {
+            $response = "Erfolgreich eingeschrieben";
+        } else {
+            $response =  "Fehler: " . $conn->error;
+        }
+    
+        // Returns response
+        header('Content-Type: application/json');
+        echo json_encode($response);
+    
+        // Closes connection
+        $conn->close();
+    }else {
+        http_response_code(400);
+        echo json_encode(array('error' => 'Ungültige Methode'));
+    }
+?>
