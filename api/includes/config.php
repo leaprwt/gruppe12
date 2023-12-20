0ab8850eb9d13b0540c344c90a2140195c335788
nanefec <116567079+nanefec@users.noreply.github.com>
2023-12-20 19:50:45 +0100

    Update config.php

--- api/includes/config.php
+++ api/includes/config.php
@@ -1,11 +1,36 @@
 <?php
-   //local enviroment
-   define("HOST", "localhost:8889");
-   define("USER", "root");
-   define("PASSWORD", "root");
+   //local env
+   // define("STUDY_PLANER_HOST", "localhost:8889");
+   // define("STUDY_PLANER_USER", "root");
+   // define("STUDY_PLANER_PASSWORD", "root");
+
+   // define("LIBRARY_HOST", "localhost:8889");
+   // define("LIBRARY_USER", "root");
+   // define("LIBRARY_PASSWORD", "root");
+
+   // define("MEAL_PLAN_HOST", "localhost:8889");
+   // define("MEAL_PLAN_USER", "root");
+   // define("MEAL_PLAN_PASSWORD", "root");
+
+   // define("STUDY_PLANER_DATABASE", "studyplaner");
+   // define("LIBRARY_DATABASE", "library");
+   // define("MEAL_PLAN_DATABASE", "mealplans");
 
    //prod env
-   // define("HOST", "m12242-30.kurs-jade-hs.de");
-   // define("USER", "m12242-30")
-   // define("PASSWORD", "adHUcO_8A")
+   define("STUDY_PLANER_HOST", "localhost");
+   define("STUDY_PLANER_USER", "m12242-30");
+   define("STUDY_PLANER_PASSWORD", "adHUcO_8A");
+
+   define("LIBRARY_HOST", "m12242-31.kurs.jade-hs.de");
+   define("LIBRARY_USER", "m12242-31");
+   define("LIBRARY_PASSWORD", "zraEkI_WJ");
+
+   define("MEAL_PLAN_HOST", "m12242-10.kurs.jade-hs.de");
+   define("MEAL_PLAN_USER", "m12242-10");
+   define("MEAL_PLAN_PASSWORD", "ho6mgi74o");
+
+   //databases
+   define("STUDY_PLANER_DATABASE", "m12242_30");
+   define("LIBRARY_DATABASE", "m12242_31");
+   define("MEAL_PLAN_DATABASE", "m12242_10");
 ?>
