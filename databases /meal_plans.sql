c44aa54e6eb774e51277795a40f7173a79628824
nanefec <116567079+nanefec@users.noreply.github.com>
2023-12-20 21:00:03 +0100

    Update meal_plans.sql

--- databases /meal_plans.sql
+++ databases /meal_plans.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Dec 08, 2023 at 05:42 PM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
+-- Host: localhost:8889
+-- Generation Time: Dec 20, 2023 at 12:27 PM
+-- Server version: 5.7.39
+-- PHP Version: 8.2.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,19 +18,30 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `meal_plans`
+-- Database: `mealplans`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `day`
+-- Table structure for table `days`
 --
 
-CREATE TABLE `day` (
+CREATE TABLE `days` (
   `day_id` int(11) NOT NULL,
   `day_name` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `days`
+--
+
+INSERT INTO `days` (`day_id`, `day_name`) VALUES
+(1, 'Montag'),
+(2, 'Dienstag'),
+(3, 'Mittwoch'),
+(4, 'Donnerstag'),
+(5, 'Freitag');
 
 -- --------------------------------------------------------
 
@@ -41,7 +52,17 @@ CREATE TABLE `day` (
 CREATE TABLE `ingredients` (
   `ingredients_id` int(32) NOT NULL,
   `ingredients_name` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `ingredients`
+--
+
+INSERT INTO `ingredients` (`ingredients_id`, `ingredients_name`) VALUES
+(1, 'Milch'),
+(2, 'Weizen'),
+(3, 'Soja'),
+(4, 'Lactose');
 
 -- --------------------------------------------------------
 
@@ -53,7 +74,28 @@ CREATE TABLE `linkingmealday` (
   `id` int(11) NOT NULL,
   `meal_id` int(32) NOT NULL,
   `day_id` int(32) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `linkingmealday`
+--
+
+INSERT INTO `linkingmealday` (`id`, `meal_id`, `day_id`) VALUES
+(18, 12, 1),
+(19, 12, 4),
+(20, 12, 5);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `linkingMealIngredients`
+--
+
+CREATE TABLE `linkingMealIngredients` (
+  `linking_id` int(11) NOT NULL,
+  `ingredient_id` int(11) NOT NULL,
+  `meal_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -63,7 +105,7 @@ CREATE TABLE `linkingmealday` (
 
 CREATE TABLE `meals` (
   `meal_id` int(32) NOT NULL,
-  `price` int(32) NOT NULL,
+  `price` float NOT NULL,
   `calories` int(32) NOT NULL,
   `protein` int(32) NOT NULL,
   `fat` int(32) NOT NULL,
@@ -71,8 +113,15 @@ CREATE TABLE `meals` (
   `name` varchar(255) NOT NULL,
   `type_of_meal_id` int(32) NOT NULL,
   `type_of_nutrition_id` int(32) NOT NULL,
-  `ingredients_id` int(32) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `path_to_image` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `meals`
+--
+
+INSERT INTO `meals` (`meal_id`, `price`, `calories`, `protein`, `fat`, `carbonhydrate`, `name`, `type_of_meal_id`, `type_of_nutrition_id`, `path_to_image`) VALUES
+(12, 4.75, 342, 32, 21, 27, 'Testmeal', 1, 1, 'app/assets/meals/image_6582be519f1321.72992020.jpeg');
 
 -- --------------------------------------------------------
 
@@ -83,7 +132,16 @@ CREATE TABLE `meals` (
 CREATE TABLE `mealtype` (
   `meal_type_id` int(32) NOT NULL,
   `name` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `mealtype`
+--
+
+INSERT INTO `mealtype` (`meal_type_id`, `name`) VALUES
+(1, 'Hauptgericht'),
+(2, 'Beilage'),
+(3, 'Dessert');
 
 -- --------------------------------------------------------
 
@@ -94,16 +152,25 @@ CREATE TABLE `mealtype` (
 CREATE TABLE `nutritiontypes` (
   `nutrition_type_id` int(32) NOT NULL,
   `nutrition_name` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `nutritiontypes`
+--
+
+INSERT INTO `nutritiontypes` (`nutrition_type_id`, `nutrition_name`) VALUES
+(1, 'Vegan'),
+(2, 'Vegetarisch'),
+(3, 'Omnivore');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `day`
+-- Indexes for table `days`
 --
-ALTER TABLE `day`
+ALTER TABLE `days`
   ADD PRIMARY KEY (`day_id`);
 
 --
@@ -117,8 +184,17 @@ ALTER TABLE `ingredients`
 --
 ALTER TABLE `linkingmealday`
   ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `unique_combination` (`meal_id`,`day_id`),
   ADD KEY `linking-meal` (`meal_id`),
   ADD KEY `linking-day` (`day_id`);
+
+--
+-- Indexes for table `linkingMealIngredients`
+--
+ALTER TABLE `linkingMealIngredients`
+  ADD PRIMARY KEY (`linking_id`),
+  ADD UNIQUE KEY `unique_combination` (`meal_id`,`ingredient_id`),
+  ADD KEY `ingredient_id` (`ingredient_id`);
 
 --
 -- Indexes for table `meals`
@@ -126,8 +202,7 @@ ALTER TABLE `linkingmealday`
 ALTER TABLE `meals`
   ADD PRIMARY KEY (`meal_id`),
   ADD KEY `meal-mealType` (`type_of_meal_id`),
-  ADD KEY `meal-nutritionType` (`type_of_nutrition_id`),
-  ADD KEY `meal-ingredients` (`ingredients_id`);
+  ADD KEY `meal-nutritionType` (`type_of_nutrition_id`);
 
 --
 -- Indexes for table `mealtype`
@@ -146,40 +221,46 @@ ALTER TABLE `nutritiontypes`
 --
 
 --
--- AUTO_INCREMENT for table `day`
+-- AUTO_INCREMENT for table `days`
 --
-ALTER TABLE `day`
-  MODIFY `day_id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `days`
+  MODIFY `day_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `ingredients`
 --
 ALTER TABLE `ingredients`
-  MODIFY `ingredients_id` int(32) NOT NULL AUTO_INCREMENT;
+  MODIFY `ingredients_id` int(32) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `linkingmealday`
 --
 ALTER TABLE `linkingmealday`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+
+--
+-- AUTO_INCREMENT for table `linkingMealIngredients`
+--
+ALTER TABLE `linkingMealIngredients`
+  MODIFY `linking_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `meals`
 --
 ALTER TABLE `meals`
-  MODIFY `meal_id` int(32) NOT NULL AUTO_INCREMENT;
+  MODIFY `meal_id` int(32) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT for table `mealtype`
 --
 ALTER TABLE `mealtype`
-  MODIFY `meal_type_id` int(32) NOT NULL AUTO_INCREMENT;
+  MODIFY `meal_type_id` int(32) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `nutritiontypes`
 --
 ALTER TABLE `nutritiontypes`
-  MODIFY `nutrition_type_id` int(32) NOT NULL AUTO_INCREMENT;
+  MODIFY `nutrition_type_id` int(32) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- Constraints for dumped tables
@@ -189,16 +270,22 @@ ALTER TABLE `nutritiontypes`
 -- Constraints for table `linkingmealday`
 --
 ALTER TABLE `linkingmealday`
-  ADD CONSTRAINT `linking-day` FOREIGN KEY (`day_id`) REFERENCES `day` (`day_id`),
+  ADD CONSTRAINT `linking-day` FOREIGN KEY (`day_id`) REFERENCES `days` (`day_id`),
   ADD CONSTRAINT `linking-meal` FOREIGN KEY (`meal_id`) REFERENCES `meals` (`meal_id`);
+
+--
+-- Constraints for table `linkingMealIngredients`
+--
+ALTER TABLE `linkingMealIngredients`
+  ADD CONSTRAINT `ingredient_id` FOREIGN KEY (`ingredient_id`) REFERENCES `ingredients` (`ingredients_id`),
+  ADD CONSTRAINT `meal_id` FOREIGN KEY (`meal_id`) REFERENCES `meals` (`meal_id`);
 
 --
 -- Constraints for table `meals`
 --
 ALTER TABLE `meals`
-  ADD CONSTRAINT `meal-ingredients` FOREIGN KEY (`ingredients_id`) REFERENCES `ingredients` (`ingredients_id`),
-  ADD CONSTRAINT `meal-mealType` FOREIGN KEY (`type_of_meal_id`) REFERENCES `mealtype` (`meal_type_id`),
-  ADD CONSTRAINT `meal-nutritionType` FOREIGN KEY (`type_of_nutrition_id`) REFERENCES `nutritiontypes` (`nutrition_type_id`);
+  ADD CONSTRAINT `meal-mealType` FOREIGN KEY (`type_of_meal_id`) REFERENCES `mealtype` (`meal_type_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `meal-nutritionType` FOREIGN KEY (`type_of_nutrition_id`) REFERENCES `nutritiontypes` (`nutrition_type_id`) ON UPDATE NO ACTION;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
