016633779c71e9fe0ccccec63ab29f6e5566d199
nanefec <116567079+nanefec@users.noreply.github.com>
2023-12-20 21:01:33 +0100

    Update main.php

--- app/main.php
+++ app/main.php
@@ -3,10 +3,55 @@
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="/app/app.css" />
+    <link
+      href="https://fonts.googleapis.com/icon?family=Material+Icons"
+      rel="stylesheet"
+    />
+    <link rel="stylesheet" href="app/app.css" />
+    <link rel="stylesheet" href="app/navbar/navbar.css" />
+    <link rel="stylesheet" href="app/main.css"/>
     <title>Study Companion | Home</title>
   </head>
   <body>
-    <h1>Homepage</h1>
+    <header>
+      <div id="brand"><a href="/">Study Companion</a></div>
+      <nav>
+        <!--Desktop Menu-->
+        <ul>
+          <li><a href="/studiplaner">Studiplaner</a></li>
+          <li><a href="/bibliothek">Bibliothek</a></li>
+          <li><a href="/mensaplan">Mensaplan</a></li>
+          <li><a href="/datenerfassen">Daten erfassen</a></li>
+          <li><a href="/statistik">Statistiken</a></li>
+        </ul>
+      </nav>
+      <!--Toggle Icon-->
+      <div id="hamburger-icon" onclick="toggleMobileMenu(this)">
+        <div class="bar1"></div>
+        <div class="bar2"></div>
+        <div class="bar3"></div>
+        <!--Mobile Menu-->
+        <ul class="mobile-menu">
+          <li><a href="/studiplaner">Studiplaner</a></li>
+          <li><a href="/bibliothek">Bibliothek</a></li>
+          <li><a href="/mensaplan">Mensaplan</a></li>
+          <li><a href="/datenerfassen">Daten erfassen</a></li>
+          <li><a href="/statistik">Statistiken</a></li>
+        </ul>
+      </div>
+    </header>
+    <main>
+      <?php
+          //imports utils
+          require_once "api/includes/config.php";
+          require_once "api/includes/connectDB.php";
+
+          //include files
+          include_once('app/studyPlaner/homeTable.inc.php');
+          include_once('app/library/homeTable.inc.php');
+          include_once('app/mealPlan/homeTable.inc.php');
+        ?>
+    </main>
+    <script src="app/navbar/navbar.js"></script>
   </body>
 </html>
