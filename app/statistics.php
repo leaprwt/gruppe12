56bddf9dde5e66084fea07068f9f86f03c5bc466
nanefec <116567079+nanefec@users.noreply.github.com>
2023-12-20 20:53:44 +0100

    Create statistics.php

---
+++ app/statistics.php
@@ -0,0 +1,73 @@
+<?php
+        //imports utils
+        require_once "api/includes/config.php";
+        require_once "api/includes/connectDB.php";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link
+      href="https://fonts.googleapis.com/icon?family=Material+Icons"
+      rel="stylesheet"
+    />
+    <link rel="stylesheet" href="/app/app.css" />
+    <link rel="stylesheet" href="/app/navbar/navbar.css" />
+    <link rel="stylesheet" href="/app/statistics.css" />
+    <title>Study Companion | Statistiken</title>
+</head>
+<body>
+<header>
+      <div id="brand"><a href="/">Study Companion</a></div>
+      <nav>
+        <!--Desktop Menu-->
+        <ul>
+          <li><a href="/studiplaner">Studiplaner</a></li>
+          <li><a href="/bibliothek">Bibliothek</a></li>
+          <li><a href="/mensaplan">Mensaplan</a></li>
+          <li><a href="/datenerfassen">Daten erfassen</a></li>
+          <li><a href="/statistik">Statistiken</a></li>
+        </ul>
+      </nav>
+      <!--Toggle Icon-->
+      <div id="hamburger-icon" onclick="toggleMobileMenu(this)">
+        <div class="bar1"></div>
+        <div class="bar2"></div>
+        <div class="bar3"></div>
+        <!--Mobile Menu-->
+        <ul class="mobile-menu">
+          <li><a href="/studiplaner">Studiplaner</a></li>
+          <li><a href="/bibliothek">Bibliothek</a></li>
+          <li><a href="/mensaplan">Mensaplan</a></li>
+          <li><a href="/datenerfassen">Daten erfassen</a></li>
+          <li><a href="/statistik">Statistiken</a></li>
+        </ul>
+      </div>
+    </header>
+
+    <main>
+        <div>
+            <h2>Statistik | Studiplaner</h2>
+            <?php
+                require_once "app/studyPlaner/statistics.inc.php";
+            ?>
+        </div>
+        <div>
+            <h2>Statistik | Bibliothek</h2>
+            <?php
+                require_once "app/library/statistics.inc.php";
+            ?>
+        </div>
+        <div>
+            <h2>Statistik | Mensaplan</h2>
+            <?php
+                require_once "app/mealPlan/statistics.inc.php";
+            ?>
+        </div>
+    </main>
+
+    <script src="/app/navbar/navbar.js"></script>
+</body>
+</html>
