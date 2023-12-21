b73228c1da1ee14542dc8f31bd24fc0e4aa95466
leaprwt <116566313+leaprwt@users.noreply.github.com>
2023-12-21 11:05:24 +0100

    Update overview.js

--- app/studyplaner/overview.js
+++ app/studyplaner/overview.js
@@ -1,5 +1,5 @@
 //fetch all courses from api
-const BASE_URL = 'http://localhost:8888';
+const BASE_URL = 'http://m12242-30.kurs.jade-hs.de';
 async function fetchData() {
   try {
     const response = await fetch(`${BASE_URL}/api/getCoursesOverview`);
