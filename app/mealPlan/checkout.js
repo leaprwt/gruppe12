101c99f3a4d7e32a03ae51e571f095a2ea8b1d5b
nanefec <116567079+nanefec@users.noreply.github.com>
2023-12-21 11:38:46 +0100

    Update checkout.js

--- app/mealPlan/checkout.js
+++ app/mealPlan/checkout.js
@@ -22,6 +22,7 @@ var items = shopping_card.reduce(function (acc, item) {
 }, {});
 //convert in array
 var resultArray = Object.values(items);
+
 //render based on new shopping data array
 const parentNode = document.querySelector('.grid-checkout-container');
 resultArray.forEach((item) => {
@@ -34,7 +35,27 @@ resultArray.forEach((item) => {
   parentNode.appendChild(mealElement);
 });
 
+//render total price
 const totalPrice = document.createElement('p');
 totalPrice.innerHTML = 'Summe: ' + localStorage.getItem('checkout_price') + '€';
 totalPrice.style.marginTop = '4rem';
 parentNode.appendChild(totalPrice);
+
+function checkoutOrder() {
+  //generate unique order number and delete local storage items if student number exists
+  const inputField = document
+    .querySelector('.student-number-input')
+    .querySelector('input');
+
+  if (inputField.value != '') {
+    alert('Deine Bestellung ist reserviert!');
+    //remove shopping card data after checkout is done
+    localStorage.setItem('shopping_card', []);
+    localStorage.setItem('checkout_price', 0);
+
+    //redirect to main page
+    window.location.href = 'http://m12242-30.kurs.jade-hs.de/mensaplan';
+  } else {
+    alert('Bitte gebe eine gültige Matrikelnummer ein!');
+  }
+}
