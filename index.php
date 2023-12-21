6fb4564da40fb9b45447db73d4e51deca294302d
leaprwt <116566313+leaprwt@users.noreply.github.com>
2023-12-21 11:08:14 +0100

    Update index.php

--- index.php
+++ index.php
@@ -9,31 +9,41 @@ if ($is_api_request) {
 } else {
     // Frontend Routes
     $frontend_routes = array(
-        '/studiplaner' => 'app/studyplaner/overview.php',
-        '/kursdetails' => 'app/studyplaner/courseDetails.php',
+        '/' => 'app/main.php',
+        '/studiplaner' => 'app/studyPlaner/overview.php',
+        '/kursdetails' => 'app/studyPlaner/courseDetails.php',
         '/bibliothek' => 'app/library/overview.php',
         '/buchdetails' =>  'app/library/bookDetails.php',
         '/mensaplan' => 'app/mealPlan/overview.php',
         '/gerichtdetails' => 'app/mealPlan/mealDetails.php',
-        '/bestellen' => 'app/mealPlan/checkout.html',
-        '/datenerfassen' => 'app/addData.html'
+        '/bestellen' => 'app/mealPlan/checkout.php',
+        '/datenerfassen' => 'app/addData.php',
+        '/statistik' => 'app/statistics.php',
     );
-
+    $found_route = false;
     foreach ($frontend_routes as $route => $file) {
-        if (strpos($path, $route) === 0) {
+        if ($path === $route) {
+            $found_route = true;
             $frontend_path = __DIR__ . '/' . $file;
 
             if (file_exists($frontend_path)) {
-                readfile($frontend_path);
+                require_once($frontend_path);
             } else {
                 // Route not found
                 http_response_code(404);
                 echo '404 Not Found';
+                echo __DIR__ . '/' . $file;
             }
 
            //End Loop
             break;
         }
+    }
+    // Check if no route was found
+    if (!$found_route) {
+        // Route not found
+        http_response_code(404);
+        echo '404 Not Found';
     }
 }
 ?>
